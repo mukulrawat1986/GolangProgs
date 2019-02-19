8c5e2248aba7815216cc8dfdfd002f96ee53f7ad
mukulrawat1986 <mukulsrawat@gmail.com>
2019-02-19 12:20:31 +0530

    reusing timeHandler in multiple routes

--- ReqHandling/custom-handler/main.go
+++ ReqHandling/custom-handler/main.go
@@ -18,11 +18,15 @@ func (th *timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	th := &timeHandler{
+	th1123 := &timeHandler{
 		format: time.RFC1123,
 	}
+	mux.Handle("/time/rfc1123", th1123)
 
-	mux.Handle("/time", th)
+	th3339 := &timeHandler{
+		format: time.RFC3339,
+	}
+	mux.Handle("/time/rfc3339", th3339)
 
 	log.Println("Listening.....")
 	http.ListenAndServe(":3000", mux)
