1613b0ff25b7ceab6ac4f27ba87c61abd7cbde53
mukulrawat1986 <mukulsrawat@gmail.com>
2019-03-05 10:35:48 +0530

    refactor Hello function and use constants

--- TestingGo/hello/hello.go
+++ TestingGo/hello/hello.go
@@ -4,6 +4,8 @@ import "fmt"
 
 const (
 	englishHelloPrefix = "Hello, "
+	french             = "French"
+	frenchHelloPrefix  = "Bonjour, "
 	spanish            = "Spanish"
 	spanishHelloPrefix = "Hola, "
 )
@@ -15,8 +17,8 @@ func Hello(name, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name
-	} else if language == "French" {
-		return "Bonjour, " + name
+	} else if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
