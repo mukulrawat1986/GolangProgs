baa05a59c86c3b30f30bdc8369180c646ab7d834
mukulrawat1986 <mukulsrawat@gmail.com>
2019-03-12 09:54:54 +0530

    remove the perimeter function and its test

--- TestingGo/StructsInterface/shapes.go
+++ TestingGo/StructsInterface/shapes.go
@@ -5,10 +5,6 @@ type Rectangle struct {
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
-}
-
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
 }
--- TestingGo/StructsInterface/shapes_test.go
+++ TestingGo/StructsInterface/shapes_test.go
@@ -2,16 +2,6 @@ package StructsInterface
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got '%.2f' want '%.2f'", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
 	rectangle := Rectangle{12.0, 6.0}
 	got := Area(rectangle)
