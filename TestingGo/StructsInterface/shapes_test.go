198d078e1ce614ec278b6020db5eeae9d1682a7f
mukulrawat1986 <mukulsrawat@gmail.com>
2019-03-12 09:55:50 +0530

    convert our test for area of rectangle into a subtest

--- TestingGo/StructsInterface/shapes_test.go
+++ TestingGo/StructsInterface/shapes_test.go
@@ -3,11 +3,15 @@ package StructsInterface
 import "testing"
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got '%.2f' want '%.2f'", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got '%.2f' want '%.2f'", got, want)
+		}
+	})
+
 }
