5a6174bc6bf4fbbe2881e961504cb84e460f9e95
mukulrawat1986 <mukulsrawat@gmail.com>
2019-04-04 16:25:12 +0530

    convert our test to a sub test

--- TestingGo/hello/hello_test.go
+++ TestingGo/hello/hello_test.go
@@ -3,10 +3,13 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
 }
