aeb17813f0b305a03d759f119efe0055fc9290ee
mukulrawat1986 <mukulsrawat@gmail.com>
2019-04-04 20:47:44 +0530

    refactor Hello function and use Switch

--- TestingGo/hello/hello.go
+++ TestingGo/hello/hello.go
@@ -14,12 +14,16 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	} else if language == french {
-		return frenchHelloPrefix + name
+
+	prefix := englishHelloPrefix
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
 	}
-	return englishHelloPrefix + name
+
+	return prefix + name
 }
 
 func main() {
