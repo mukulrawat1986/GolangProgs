187a23703da0c774c944659d0dcb3b6ca944f9fb
mukulrawat1986 <mukulsrawat@gmail.com>
2019-04-14 00:05:49 +0530

    move our assertion into a separate helper function

--- TestingGo/arrays/sum_test.go
+++ TestingGo/arrays/sum_test.go
@@ -1,28 +1,25 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
+	assertCorrectSum := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%d' want '%d', given '%v'", got, want, numbers)
 		}
+	}
+
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		assertCorrectSum(t, Sum(numbers), 15, numbers)
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got '%d' want '%d', given '%v'", got, want, numbers)
-		}
+		assertCorrectSum(t, Sum(numbers), 6, numbers)
 	})
 }
