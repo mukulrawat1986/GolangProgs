b895d94d7007e00c092c6678f68baabbae3a8dff
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-01 00:26:23 +0530

    refactor and use constants

--- TestingGo/hello/hello.go
+++ TestingGo/hello/hello.go
@@ -3,15 +3,17 @@ package main
 import "fmt"
 
 const (
+	spanish            = "Spanish"
 	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
 )
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish" {
-		return "Hola, " + name
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
