ad6ad099e9298162ec568441914146d383b4bbe5
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-01 00:46:36 +0530

    refactor the Hello function and pass the test

--- TestingGo/hello/hello.go
+++ TestingGo/hello/hello.go
@@ -14,6 +14,8 @@ func Hello(name, language string) string {
 	}
 	if language == spanish {
 		return spanishHelloPrefix + name
+	} else if language == "French" {
+		return "Bonjour, " + name
 	}
 	return englishHelloPrefix + name
 }
