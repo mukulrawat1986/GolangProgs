5d895c18ec831ddd5ef085f0da1b67b22b2edb48
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-01 00:24:04 +0530

    check if the language is Spanish, if so change the greeting

--- TestingGo/hello/hello.go
+++ TestingGo/hello/hello.go
@@ -10,6 +10,9 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
 	return englishHelloPrefix + name
 }
 
