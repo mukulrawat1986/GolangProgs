71eec21fb7cf991384722241280445f9d35a9bf0
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-01 22:15:23 +0530

    refactor our function and use range based for loop

--- TestingGo/arrayslice/sum.go
+++ TestingGo/arrayslice/sum.go
@@ -3,8 +3,8 @@ package main
 // Sum will take an array of numbers and return the sum
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
