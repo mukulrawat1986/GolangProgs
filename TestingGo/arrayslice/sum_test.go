b5b667c031a204704e5722a627e7490758333572
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-01 22:24:14 +0530

    move our assertions into a separate function

--- TestingGo/arrayslice/sum_test.go
+++ TestingGo/arrayslice/sum_test.go
@@ -4,25 +4,24 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
+	assertCorrectMessage := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	}
+
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
-		want := 15
-
-		if want != got {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
+		assertCorrectMessage(t, got, 15, numbers)
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
+		assertCorrectMessage(t, got, 6, numbers)
 	})
 }
