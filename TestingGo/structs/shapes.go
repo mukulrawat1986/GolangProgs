52b0974c7b76d1f9c767989a41a895041d3c2e5f
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-16 01:29:14 +0530

    Declare a new Triangle type that implements Shape and write tests for it

--- TestingGo/structs/shapes.go
+++ TestingGo/structs/shapes.go
@@ -18,6 +18,12 @@ type Circle struct {
 	Radius float64
 }
 
+// Triangle type to represent a triangle
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 // Area method on Rectangle returns the area of a rectangle
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
@@ -26,4 +32,9 @@ func (r Rectangle) Area() float64 {
 // Area method on Circle returns the area of the circle
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+// Area method on Triangle returns the area of the triangle
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
--- TestingGo/structs/shapes_test.go
+++ TestingGo/structs/shapes_test.go
@@ -21,6 +21,13 @@ func TestArea(t *testing.T) {
 			},
 			want: 314.1592653589793,
 		},
+		{
+			shape: Triangle{
+				Base:   12.0,
+				Height: 6.0,
+			},
+			want: 36.0,
+		},
 	}
 
 	for _, tt := range areaTests {
