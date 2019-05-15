501efb07fe4bd302326e07d8cef0fcd2896a482c
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-16 01:25:49 +0530

    use table tests

--- TestingGo/structs/shapes_test.go
+++ TestingGo/structs/shapes_test.go
@@ -4,26 +4,29 @@ import "testing"
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{
+			shape: Rectangle{
+				Width:  12.0,
+				Height: 6.0,
+			},
+			want: 72.0,
+		},
+		{
+			shape: Circle{
+				Radius: 10.0,
+			},
+			want: 314.1592653589793,
+		},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{
-			Width:  12.0,
-			Height: 6.0,
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{
-			Radius: 10.0,
-		}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	}
 }
