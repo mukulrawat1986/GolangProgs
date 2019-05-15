8e32e6fa02f15c11e1324a4d909ebe6097a28fda
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-16 00:52:50 +0530

    convert our test to a subtest

--- TestingGo/structs/shapes_test.go
+++ TestingGo/structs/shapes_test.go
@@ -3,14 +3,18 @@ package shapes
 import "testing"
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{
-		Width:  12.0,
-		Height: 6.0,
-	}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{
+			Width:  12.0,
+			Height: 6.0,
+		}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+
+	})
 }
