59ad10df162eb37d32ce66b34127b5f59c86de77
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-20 14:40:10 +0530

    create a Wallet struct and declare its methods and write tests for it

---
+++ TestingGo/pointers/wallet_test.go
@@ -0,0 +1,16 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d and want %d", got, want)
+	}
+}
