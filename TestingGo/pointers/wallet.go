808bffbb0c189ecd38c9832d67d56e68fbbee837
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-20 14:49:28 +0530

    create the Bitcoin type and use it instead of int

--- TestingGo/pointers/wallet.go
+++ TestingGo/pointers/wallet.go
@@ -1,16 +1,19 @@
 package wallet
 
+// Bitcoin type based on int
+type Bitcoin int
+
 // Wallet type to hold our bitcoins
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit method deposits bitcoins in our bitcoin
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance method returns the number of bitcoins in our wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
