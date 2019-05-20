59853b37f1facef32063937139b2915eeea25d2f
mukulrawat1986 <mukulsrawat@gmail.com>
2019-05-20 14:47:28 +0530

    add comments

--- TestingGo/pointers/wallet.go
+++ TestingGo/pointers/wallet.go
@@ -1,13 +1,16 @@
 package wallet
 
+// Wallet type to hold our bitcoins
 type Wallet struct {
 	balance int
 }
 
+// Deposit method deposits bitcoins in our bitcoin
 func (w *Wallet) Deposit(amount int) {
 	w.balance += amount
 }
 
+// Balance method returns the number of bitcoins in our wallet
 func (w *Wallet) Balance() int {
 	return w.balance
 }
