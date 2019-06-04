f0b05b92a34011464243dcc67203f393e2fea394
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-04 15:37:25 +0530

    showing byte positions and len of string in a utf-8 string

--- IntroGo/strings/main.go
+++ IntroGo/strings/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	atoz := "the quick brown fox jumps over the lazy dog\n"
+	//  atoz := "the quick brown fox jumps over the lazy dog\n"
 
-	// make substrings
-	fmt.Printf("%s\n", atoz[:9])
-	fmt.Printf("%s\n", atoz[15:19])
-	fmt.Printf("%s\n", atoz[15:])
+	temp := "こんにちは世界"
+
+	fmt.Println(len(temp))
+
+	// go through a string rune by rune
+	for i, r := range temp {
+		fmt.Printf("%d %c\n", i, r)
+	}
 }
