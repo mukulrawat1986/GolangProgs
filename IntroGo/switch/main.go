a1c03273e6ef6f2ddbff463bdaf5d6e580dfa83d
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-06 12:34:12 +0530

    switching over a variable and fallthrough

--- IntroGo/switch/main.go
+++ IntroGo/switch/main.go
@@ -18,4 +18,23 @@ func main() {
 	default:
 		fmt.Printf("OK!\n")
 	}
+
+	// switching over a variable
+	atoz := "the quick brown fox jumps over the lazy dog"
+
+	vowels, consonants, zeds := 0, 0, 0
+
+	for _, r := range atoz {
+		switch r {
+		case 'a', 'e', 'i', 'o', 'u':
+			vowels += 1
+		case 'z':
+			zeds += 1
+			fallthrough
+		default:
+			consonants += 1
+		}
+	}
+
+	fmt.Printf("Vowels: %d; Consonants: %d (Zeds: %d)\n", vowels, consonants, zeds)
 }
