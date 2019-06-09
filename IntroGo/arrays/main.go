f4146b8ec22b439fcb36cac880b0a3d3f4309a29
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-09 14:59:16 +0530

    copy a slice

--- IntroGo/arrays/main.go
+++ IntroGo/arrays/main.go
@@ -15,15 +15,15 @@ func main() {
 	// and capacity of 4
 	words := make([]string, 0, 4)
 
-	fmt.Printf("%d %d\n", len(words), cap(words))
 	words = append(words, "The")
 	words = append(words, "Quick")
 	words = append(words, "Brown")
 	words = append(words, "Fox")
-	fmt.Printf("%d %d\n", len(words), cap(words))
 
 	printer(words)
 
-	words = append(words, "Jumps")
-	fmt.Printf("%d %d\n", len(words), cap(words))
+	newWords := make([]string, 4)
+	copy(newWords, words)
+	printer(newWords)
+
 }
