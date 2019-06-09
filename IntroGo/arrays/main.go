45cf95b5b92cd4d96ba4191b39aff869c41e7604
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-09 13:45:33 +0530

    example showing length and capacity of slice

--- IntroGo/arrays/main.go
+++ IntroGo/arrays/main.go
@@ -11,12 +11,19 @@ func printer(w []string) {
 
 func main() {
 	// creating a slice and adding strings to it
-	words := make([]string, 4)
+	// create a slice of 0 length (means there is nothing in it),
+	// and capacity of 4
+	words := make([]string, 0, 4)
 
-	words[0] = "The"
-	words[1] = "Quick"
-	words[2] = "Brown"
-	words[3] = "Fox"
+	fmt.Printf("%d %d\n", len(words), cap(words))
+	words = append(words, "The")
+	words = append(words, "Quick")
+	words = append(words, "Brown")
+	words = append(words, "Fox")
+	fmt.Printf("%d %d\n", len(words), cap(words))
 
 	printer(words)
+
+	words = append(words, "Jumps")
+	fmt.Printf("%d %d\n", len(words), cap(words))
 }
