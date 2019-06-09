122c73541dee55c4b05c1e0d81e272a9371c9bd6
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-09 13:30:48 +0530

    example showing pass by copy

--- IntroGo/arrays/main.go
+++ IntroGo/arrays/main.go
@@ -7,10 +7,12 @@ func printer(w [4]string) {
 		fmt.Printf("%s", word)
 	}
 	fmt.Printf("\n")
+	w[2] = "blue"
 }
 
 func main() {
 	// array with unknown number of values of strings
 	words := [...]string{"the", "quick", "brown", "fox"}
+	printer(words)
 	printer(words)
 }
