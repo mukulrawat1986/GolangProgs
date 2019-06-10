667f5cd7fde3f707817a1bf52a6ec346a6e99395
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-10 13:43:16 +0530

    example of static declaration and initialization of a map

--- IntroGo/maps/main.go
+++ IntroGo/maps/main.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	dayMonths := make(map[string]int)
-
-	dayMonths["Jan"] = 31
-	dayMonths["Feb"] = 28
-	dayMonths["Mar"] = 31
-	dayMonths["Apr"] = 30
-	dayMonths["May"] = 31
-	dayMonths["Jun"] = 30
-	dayMonths["Jul"] = 31
-	dayMonths["Aug"] = 31
-	dayMonths["Sep"] = 30
-	dayMonths["Oct"] = 31
-	dayMonths["Nov"] = 30
-	dayMonths["Dec"] = 31
+	dayMonths := map[string]int{
+		"Jan": 31,
+		"Feb": 28,
+		"Mar": 31,
+		"Apr": 30,
+		"May": 31,
+		"Jun": 30,
+		"Jul": 31,
+		"Aug": 31,
+		"Sep": 30,
+		"Oct": 31,
+		"Nov": 30,
+		"Dec": 31,
+	}
 
 	fmt.Printf("Days in Feb: %d\n", dayMonths["Feb"])
 
