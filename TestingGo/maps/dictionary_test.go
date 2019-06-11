ca5a2e5a6b09ce9796e0d9fa1860db5d62c73711
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-11 23:13:34 +0530

    convert our test to a subtest for when we add a new word to the dictionary

--- TestingGo/maps/dictionary_test.go
+++ TestingGo/maps/dictionary_test.go
@@ -22,12 +22,15 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("existing word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+
+	})
 }
 
 func assertString(t *testing.T, got string, want string) {
