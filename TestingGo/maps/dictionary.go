ade7655e1f8ad7e2f427ea6a4e70546dbeafe706
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-11 15:05:10 +0530

    convert our error into an error variable

--- TestingGo/maps/dictionary.go
+++ TestingGo/maps/dictionary.go
@@ -2,12 +2,16 @@ package maps
 
 import "errors"
 
+var (
+	ErrNotFound = errors.New("could not find the word you were looking for")
+)
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
