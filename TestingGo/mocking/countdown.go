4d740a3d85deb15d2e85af5f23090f3c7aae5adc
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-12 17:28:18 +0530

    write enough code to pass the test

--- TestingGo/mocking/countdown.go
+++ TestingGo/mocking/countdown.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
 	"io"
 	"os"
 )
 
 func Countdown(writer io.Writer) {
-
+	fmt.Print(writer, "3")
 }
 
 func main() {
