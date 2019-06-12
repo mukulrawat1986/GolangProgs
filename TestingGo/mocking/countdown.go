e047b190c2175166084d4f62ae37a57b314d6c42
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-12 17:18:54 +0530

    write test for the Countdown function which prints a countdown message.
    We declare the Countdown function and pass a buffer to it in our test so
    we can capture what data is being generated. We pass os.Stdout in our
    main function

---
+++ TestingGo/mocking/countdown.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func Countdown(writer io.Writer) {
+
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
---
+++ TestingGo/mocking/countdown_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	// create a buffer that we inject to the Countdown function
+	// This buffer is where the writing happens in our test, so we can
+	// capture what data is being generated
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	got := buffer.String()
+	want := "3"
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
