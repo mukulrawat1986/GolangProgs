81a0db84b7b8147a8ad51694d07a9c60fdaba0e0
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-12 18:13:54 +0530

    gofmt the file and use constant variables

--- TestingGo/mocking/countdown.go
+++ TestingGo/mocking/countdown.go
@@ -6,11 +6,16 @@ import (
 	"os"
 )
 
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
 func Countdown(writer io.Writer) {
-	for i:= 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
 	}
-	fmt.Fprint(writer, "Go!")
+	fmt.Fprint(writer, finalWord)
 }
 
 func main() {
