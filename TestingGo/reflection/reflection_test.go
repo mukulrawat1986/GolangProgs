feb0563029fdf168c35988e1838f59cb76570cb3
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-17 00:38:09 +0530

    write test for a slice

--- TestingGo/reflection/reflection_test.go
+++ TestingGo/reflection/reflection_test.go
@@ -95,6 +95,26 @@ func TestWalk(t *testing.T) {
 				"London",
 			},
 		},
+		{
+			Name: "slices",
+			Input: []struct{
+					Age int
+					City string
+			}{
+				{
+					Age: 33,
+					City: "London",
+				},
+				{
+					Age: 34,
+					City: "Reykjavik",
+				},
+			},
+			ExpectedCalls: []string{
+				"London",
+				"Reykjavik",
+			},
+		},
 	}
 
 
