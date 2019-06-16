8842fc0504b73b0ceff255e4d4ef32b5cdba4142
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-16 23:54:56 +0530

    write comment to make some sense

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -3,7 +3,11 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	val := reflect.ValueOf(x) // returns a new value initialized to the concrete value stored in the
+							  // interface i
+
+	field := val.Field(0) // returns the ith field of the struct, panics if its not a struct or i
+							  // is out of range
+
+	fn(field.String()) // returns the underlying value as a string, panics if its not a string
 }
