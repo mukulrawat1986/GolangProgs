fdda5bb6661c80ecfed614be82a5434a33771871
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-18 12:12:14 +0530

    make our own function and inspect the type and value stored in the empty interface

--- GOTR/reflection/ex2/main.go
+++ GOTR/reflection/ex2/main.go
@@ -1,1 +1,33 @@
-package ex2
+package main
+
+import "fmt"
+
+type (
+	ID     string
+	Person struct {
+		name string
+	}
+)
+
+func main() {
+	Println(true)
+	Println(2010)
+	Println(9.14)
+	Println(7 + 7i)
+	Println("Hello, World!")
+	Println(ID("fkdjfd"))
+	Println([5]byte{})
+	Println([]byte{})
+	Println(map[string]int{})
+	Println(Person{name: "Anna"})
+	Println(&Person{name: "Anna"})
+	Println(make(chan int))
+	Println(nil)
+	var i interface{}
+
+	Println(i)
+}
+
+func Println(x interface{}) {
+	fmt.Printf("type is '%T', value is '%v'\n", x, x)
+}
