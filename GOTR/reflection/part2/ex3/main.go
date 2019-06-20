3f963ad10c4ddc7fa471ee5b1fb50e6a62d5b547
mukulrawat1986 <mukulsrawat@gmail.com>
2019-06-20 23:08:10 +0530

    more examples of Kind of a type

--- GOTR/reflection/part2/ex3/main.go
+++ GOTR/reflection/part2/ex3/main.go
@@ -22,4 +22,17 @@ func main() {
 	// What kind or category is the type a member of?
 	fmt.Printf("t0: type = %v, kind = %v\n", t0, t0.Kind())
 	fmt.Printf("t1: type = %v, kind = %v\n", t1, t1.Kind())
+
+	x = [5]int{}
+	fmt.Printf("x: type = %v, kind = %v\n", reflect.TypeOf(x), reflect.TypeOf(x).Kind())
+
+	x = [10]float32{}
+	fmt.Printf("x: type = %v, kind = %v\n", reflect.TypeOf(x), reflect.TypeOf(x).Kind())
+
+	x = []int{}
+	fmt.Printf("x: type = %v, kind = %v\n", reflect.TypeOf(x), reflect.TypeOf(x).Kind())
+
+	x = []string{}
+	fmt.Printf("x: type = %v, kind = %v\n", reflect.TypeOf(x), reflect.TypeOf(x).Kind())
+
 }
