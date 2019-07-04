1e64cd2427da4a90e0a3fdccef576626504dd503
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-04 21:26:55 +0530

    move our assertions into a separate function

--- gopl.io/ch1/helloworld/main_test.go
+++ gopl.io/ch1/helloworld/main_test.go
@@ -7,27 +7,22 @@ import (
 
 func TestHello(t *testing.T) {
 
-	t.Run("when a name is passed", func(t *testing.T) {
-		buffer := &bytes.Buffer{}
-		Hello(buffer, "Chris")
-
-		got := buffer.String()
-		want := "Hello, Chris"
-
+	assertStrings := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
+	}
+
+	t.Run("when a name is passed", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Hello(buffer, "Chris")
+		assertStrings(t, buffer.String(), "Hello, Chris")
 	})
 
 	t.Run("when no name is passed to the function", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
 		Hello(buffer, "")
-
-		got := buffer.String()
-		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertStrings(t, buffer.String(), "Hello, World")
 	})
 }
