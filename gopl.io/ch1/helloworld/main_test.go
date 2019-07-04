43b2d8f39054988d2d96d374bc09aca50ba66ec5
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-04 21:13:39 +0530

    write test for when no string is passed to the function

--- gopl.io/ch1/helloworld/main_test.go
+++ gopl.io/ch1/helloworld/main_test.go
@@ -18,4 +18,16 @@ func TestHello(t *testing.T) {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
 	})
+
+	t.Run("when no name is passed to the function", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Hello(buffer, "")
+
+		got := buffer.String()
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
 }
