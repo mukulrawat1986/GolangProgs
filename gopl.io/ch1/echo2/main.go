701e7757904db10eca3d594711c1fb5b2920677e
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-05 23:51:01 +0530

    implement the function and pass the test, use '%q' in our test

--- gopl.io/ch1/echo2/main.go
+++ gopl.io/ch1/echo2/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 )
 
 func Echo(args []string, w io.Writer) {
+	s, sep := "", ""
+	for _, arg := range args[1:] {
+		s += sep + arg
+		sep = " "
+	}
 
+	fmt.Fprintf(w, "%s\n", s)
 }
 
 func main() {
