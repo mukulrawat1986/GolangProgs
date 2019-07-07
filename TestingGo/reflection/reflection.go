50b8ee60e85e448003e26a212d54cde871c3f535
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-07 06:49:02 +0530

    write some reall naive and unsafe code and pass the test

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -1,7 +1,17 @@
 package main
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat to put them last in their group")
+
+	// get the underlying concrete valye stored in the interface
+	value := reflect.ValueOf(x)
+
+	// get the first and only field
+	field := value.Field(0)
+
+	// apply the function to the string field
+	fn(field.String())
 }
 
 func main() {
