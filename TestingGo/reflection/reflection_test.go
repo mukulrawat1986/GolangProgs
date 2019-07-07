8546f0a7884b1eeda3ad6c286166c0c308a8f1a4
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-07 07:03:15 +0530

    write test for the scenario when there's a non-string field in the struct

--- TestingGo/reflection/reflection_test.go
+++ TestingGo/reflection/reflection_test.go
@@ -32,6 +32,17 @@ func TestWalk(t *testing.T) {
 			},
 			ExpectedCalls: []string{"Chris", "London"},
 		},
+		{
+			Name: "Struct with non string field",
+			Input: struct {
+				Name string
+				Age  int
+			}{
+				Name: "Chris",
+				Age:  33,
+			},
+			ExpectedCalls: []string{"Chris"},
+		},
 	}
 
 	for _, test := range cases {
