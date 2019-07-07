7d0049a27f820b2e53d1c53fe0476058744dbbb7
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-07 22:33:56 +0530

    check if the value is of poniter type if so extract the value being pointed to by the pointer

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -7,6 +7,12 @@ func walk(x interface{}, fn func(input string)) {
 	// get the concrete value stored in the interface
 	val := reflect.ValueOf(x)
 
+	// check if the value is of pointer type, if so extract the value that
+	// the pointer points to
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
 	// iterate over the number of fields stored in x, assuming x is a struct
 	for i := 0; i < val.NumField(); i++ {
 		// get the ith field of the struct
