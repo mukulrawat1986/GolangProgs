93ce3df89cb9f44aa5d748fa0ddf12d766e3d505
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-07 06:58:00 +0530

    write test for the case when the struct contains two strings

--- TestingGo/reflection/reflection_test.go
+++ TestingGo/reflection/reflection_test.go
@@ -21,6 +21,17 @@ func TestWalk(t *testing.T) {
 			},
 			ExpectedCalls: []string{"Chris"},
 		},
+		{
+			Name: "Struct with two string fields",
+			Input: struct {
+				Name string
+				City string
+			}{
+				Name: "Chris",
+				City: "London",
+			},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
 	}
 
 	for _, test := range cases {
