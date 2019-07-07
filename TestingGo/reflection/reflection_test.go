c9979da0ca43cf697ccadc967b6a2a5bf42fae94
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-07 11:32:34 +0530

    write test for the case when we have a nested struct

--- TestingGo/reflection/reflection_test.go
+++ TestingGo/reflection/reflection_test.go
@@ -43,6 +43,26 @@ func TestWalk(t *testing.T) {
 			},
 			ExpectedCalls: []string{"Chris"},
 		},
+		{
+			Name: "Struct with nested fields",
+			Input: struct {
+				Name    string
+				Profile struct {
+					Age  int
+					City string
+				}
+			}{
+				Name: "Chris",
+				Profile: struct {
+					Age  int
+					City string
+				}{
+					Age:  33,
+					City: "London",
+				},
+			},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
 	}
 
 	for _, test := range cases {
