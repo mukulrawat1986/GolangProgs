7dac20306803e1a2a33ae24f8557467f10ad1d83
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-07 07:01:09 +0530

    refactor the code and loop over the number of fields

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -4,14 +4,17 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 
-	// get the underlying concrete valye stored in the interface
-	value := reflect.ValueOf(x)
+	// get the concrete value stored in the interface
+	val := reflect.ValueOf(x)
 
-	// get the first and only field
-	field := value.Field(0)
+	// we are assuming its a struct and looping over the number of fields
+	for i := 0; i < val.NumField(); i++ {
+		// get the individual field value
+		field := val.Field(i)
 
-	// apply the function to the string field
-	fn(field.String())
+		// apply the function to this field, assuming its a string
+		fn(field.String())
+	}
 }
 
 func main() {
