dfab15e657dd471b3c4ca2671fdaafffa5c7322e
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-08 06:40:08 +0530

    refactor our code

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -4,32 +4,26 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 
-	// extract the concrete value from inside the interface
+	// extract the underlying concrete value from the interface
 	val := getValue(x)
 
-	// check if our value is a slice, if so call walk recursively for each slice element
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+
+	// if its a struct, call walk on each field in struct
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+
+	// if its a slice, call walk on each index in the slice
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
 
-	// iterate over the number of fields stored in x, assuming x is a struct
-	for i := 0; i < val.NumField(); i++ {
-		// get the ith field of the struct
-		field := val.Field(i)
-
-		// switch on the kind of field
-		switch field.Kind() {
-		case reflect.String:
-			// apply the function
-			fn(field.String())
-
-		case reflect.Struct:
-			// recursively call the walk function
-			walk(field.Interface(), fn)
-		}
+	// if its a string, apply the function
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
