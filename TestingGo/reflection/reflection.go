5c2c71f18450fbfdfdd94c2e954877dd66166580
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-13 21:26:14 +0530

    write enough code to pass the test

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -1,5 +1,15 @@
 package reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put last in their group")
+	// get the concrete value stored in the interface x
+	val := reflect.ValueOf(x)
+
+	// get the value of the 0th field stored in the struct val,
+	// assuming val is a struct
+	field := val.Field(0)
+
+	// return the string's underlying value as a string
+	fn(field.String())
 }
