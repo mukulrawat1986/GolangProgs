f6b0d9437a310cdad063ab03087b8ae8ee40f0d8
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-19 16:29:15 +0530

    move our assertions into a separate helper function

--- TestingGo/hello/hello_test.go
+++ TestingGo/hello/hello_test.go
@@ -4,23 +4,19 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
-
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("Chris"), "Hello, Chris")
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, Hello(""), "Hello, World")
 	})
 
 }
