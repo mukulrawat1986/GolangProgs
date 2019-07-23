214558c2fd24693a5662506b1268aa7c59e8cf8d
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-23 23:19:56 +0530

    add benchmark and refactor function to accept repeat count

--- TestingGo/iteration/repeat.go
+++ TestingGo/iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
 // Repeat repeats a character
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 
--- TestingGo/iteration/repeat_test.go
+++ TestingGo/iteration/repeat_test.go
@@ -3,10 +3,19 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	t.Run("repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
 }
