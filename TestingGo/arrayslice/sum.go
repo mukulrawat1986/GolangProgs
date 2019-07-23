24eda8e58478cf9c544be150df50c2f3a98e8dd6
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-23 23:29:24 +0530

    implement the function

--- TestingGo/arrayslice/sum.go
+++ TestingGo/arrayslice/sum.go
@@ -2,7 +2,11 @@ package main
 
 // Sum returns the sum of the array
 func Sum(numbers [5]int) int {
-	return 0
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
 
 func main() {
