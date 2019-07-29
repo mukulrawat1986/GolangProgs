7b3d774d311b947af9ae0894a320d0900d60109d
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-29 18:25:26 +0530

    refactor our test

--- TestingGo/select/racer_test.go
+++ TestingGo/select/racer_test.go
@@ -8,14 +8,8 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(time.Duration(20 * time.Millisecond))
+	fastServer := makeDelayedServer(time.Duration(0 * time.Millisecond))
 
 	defer slowServer.Close()
 	defer fastServer.Close()
@@ -27,6 +21,13 @@ func TestRacer(t *testing.T) {
 	got := Racer(slowURL, fastURL)
 
 	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
