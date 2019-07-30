fc5eecdb657576954b64fdbe284967e19b850596
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-30 21:55:02 +0530

    refactor and move our balance assertion to a separate function

--- TestingGo/pointers/wallet_test.go
+++ TestingGo/pointers/wallet_test.go
@@ -4,27 +4,28 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertBalanace := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalanace(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
-		wallet := Wallet{}
+		wallet := Wallet{
+			balance: Bitcoin(20),
+		}
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalanace(t, wallet, Bitcoin(10))
 	})
 }
