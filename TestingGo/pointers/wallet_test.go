0b510c87b9c546a895917c7b320e375b14a6fa27
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-30 21:48:27 +0530

    convert our test to a subtest

--- TestingGo/pointers/wallet_test.go
+++ TestingGo/pointers/wallet_test.go
@@ -3,13 +3,17 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
