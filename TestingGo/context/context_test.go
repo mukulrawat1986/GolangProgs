80a21a9a3f2367cd8bcb7d32a514b96e266554a8
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-01 00:51:39 +0530

    make our spy satisfy the store interface and convert our test to a subtest

--- TestingGo/context/context_test.go
+++ TestingGo/context/context_test.go
@@ -4,28 +4,40 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-type StubStore struct {
-	response string
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	// add some delay
+	time.Sleep(100 * time.Millisecond)
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func TestHandler(t *testing.T) {
 	data := "Hello, World"
-	svr := Server(&StubStore{
-		response: data,
+
+	t.Run("returns data from store", func(t *testing.T) {
+		svr := Server(&SpyStore{
+			response: data,
+		})
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf("got %q want %q", response.Body.String(), data)
+		}
+
 	})
-
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-
-	svr.ServeHTTP(response, request)
-
-	if response.Body.String() != data {
-		t.Errorf("got %q want %q", response.Body.String(), data)
-	}
 }
