8fe3055aa926d6bd36d5769c7fe2a8ba86c96d0a
mukulrawat1986 <mukulsrawat@gmail.com>
2019-07-31 11:55:49 +0530

    write a server that runs a long running process to fetch data

---
+++ TestingGo/context/context.go
@@ -0,0 +1,18 @@
+package _context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+// Server starts a long running process to fetch some data
+// for it to return in response
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
