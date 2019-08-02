b61935963e92fd88d9e78f12364168fad9500588
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-03 00:55:35 +0530

    using WithTimeout

--- justforfunc/context/main.go
+++ justforfunc/context/main.go
@@ -9,9 +9,8 @@ import (
 func main() {
 	ctx := context.Background()
 	// create a child of main context and add cancellation to it
-	ctx, cancel := context.WithCancel(ctx)
-
-	time.AfterFunc(time.Second, cancel)
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
 	sleepAndTalk(ctx, 5*time.Second, "Hello, World")
 }
 
@@ -20,6 +19,7 @@ func sleepAndTalk(ctx context.Context, d time.Duration, msg string) {
 	case <-time.After(d):
 		fmt.Println(msg)
 	case <-ctx.Done():
+		fmt.Println("Test")
 		fmt.Println(ctx.Err())
 	}
 }
