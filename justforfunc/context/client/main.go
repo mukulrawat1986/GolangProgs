d0f75653142f048c24f0dd9a953e186547e931f9
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-03 11:22:12 +0530

    write a simple http client for our server

---
+++ justforfunc/context/client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	res, err := http.Get("http://localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatal(res.StatusCode)
+	}
+
+	io.Copy(os.Stdout, res.Body)
+}
