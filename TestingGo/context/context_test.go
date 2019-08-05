0a04d0fe7ee0e121f1401bb0181ea6a831c557c7
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-06 01:52:47 +0530

    write a simple test for our handler

--- TestingGo/context/context_test.go
+++ TestingGo/context/context_test.go
@@ -1,1 +1,31 @@
 package _context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestHandler(t *testing.T) {
+	data := "Hello, World"
+	svr := Server(&StubStore{
+		response: data,
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf("got %q want %q", response.Body.String(), data)
+	}
+}
