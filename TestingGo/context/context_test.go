b5badc931e00c02a29c938570bad0f58d4137d80
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-06 12:18:39 +0530

    write test to check if work is cancelled when request is cancelled

--- TestingGo/context/context_test.go
+++ TestingGo/context/context_test.go
@@ -1,6 +1,7 @@
 package _context
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -23,11 +24,15 @@ func (s *SpyStore) Cancel() {
 
 func TestHandler(t *testing.T) {
 
+	data := "Hello, World"
+
 	t.Run("returns data from store", func(t *testing.T) {
-		data := "Hello, World"
-		svr := Server(&SpyStore{
-			response: data,
-		})
+		store := &SpyStore{
+			response:  "Hello, World",
+			cancelled: false,
+		}
+		svr := Server(store)
+
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
 		response := httptest.NewRecorder()
@@ -40,4 +45,25 @@ func TestHandler(t *testing.T) {
 
 	})
 
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{
+			response:  data,
+			cancelled: false,
+		}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		ctx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5 * time.Millisecond, cancel)
+
+		request = request.WithContext(ctx)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("store was not told to cancel")
+		}
+	})
 }
