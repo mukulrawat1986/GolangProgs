01c50f191b8227c4809a4871efffa7dc3d205548
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-06 14:37:05 +0530

    update out store interface and handler

--- TestingGo/context/context.go
+++ TestingGo/context/context.go
@@ -1,35 +1,15 @@
 package _context
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// get context from request
-		ctx := r.Context()
-
-		// create a channel to send data in
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-
-		case <-ctx.Done():
-			// request is cancelled
-			store.Cancel()
-		}
-
 	}
 }
