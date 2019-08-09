4a5db9fa63a8a113f20d5ffd3357e4b663a6e62a
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-10 02:26:33 +0530

    hardcode an inmemory store

--- TestingGo/serverex/main.go
+++ TestingGo/serverex/main.go
@@ -7,9 +7,17 @@ import (
 	"github.com/mukulrawat1986/GolangProgs/TestingGo/serverex/server"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
 
-	server := &server.PlayerServer{}
+	server := &server.PlayerServer{
+		Store: &InMemoryPlayerStore{},
+	}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
