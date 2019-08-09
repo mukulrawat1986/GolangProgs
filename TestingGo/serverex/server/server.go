3ee9f0ad89c1eb3da350c13e1dd3ee33da6b8967
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-10 01:48:11 +0530

    create a PlayerStore interface and convert PlayerServer to a type with state

--- TestingGo/serverex/server/server.go
+++ TestingGo/serverex/server/server.go
@@ -5,18 +5,15 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
-func GetPlayerScore(player string) string {
-	switch player {
-	case "Pepper":
-		return "20"
-	case "Floyd":
-		return "10"
-	default:
-		return ""
-	}
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
