67d95a9090c5e00280abbcbb5494cf5be0aed62b
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-10 09:39:48 +0530

    update our test and test for StatusOK in our first two tests

--- TestingGo/serverex/server/server_test.go
+++ TestingGo/serverex/server/server_test.go
@@ -34,6 +34,7 @@ func TestGetPlayer(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
@@ -43,6 +44,7 @@ func TestGetPlayer(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "10")
 	})
 
@@ -52,12 +54,7 @@ func TestGetPlayer(t *testing.T) {
 
 		server.ServeHTTP(response, request)
 
-		got := response.Code
-		want := http.StatusNotFound
-
-		if got != want {
-			t.Errorf("got status %d want %d", got, want)
-		}
+		assertStatus(t, response.Code, http.StatusNotFound)
 	})
 }
 
@@ -70,5 +67,12 @@ func assertResponseBody(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("reponse body is wrong, got %q want %q", got, want)
+	}
+}
+
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got status %d want %d", got, want)
 	}
 }
