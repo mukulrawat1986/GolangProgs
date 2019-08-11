affc6adcd124b7941324d602cb9fe44d427815db
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-11 08:01:43 +0530

    write integration test

--- TestingGo/serverex/main_test.go
+++ TestingGo/serverex/main_test.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mukulrawat1986/GolangProgs/TestingGo/serverex/server"
+)
+
+// Integration test
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := InMemoryPlayerStore{}
+	server := server.PlayerServer{
+		Store: &store,
+	}
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
+
+func newPostWinRequest(name string) *http.Request {
+	req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("reponse body is wrong, got %q want %q", got, want)
+	}
+}
+
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got status %d want %d", got, want)
+	}
+}
+
