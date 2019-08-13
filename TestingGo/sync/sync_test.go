749af294ead30b80090d8f0ccf92ae872fc3f2de
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-13 19:04:19 +0530

    write a similar test that breaks our code

--- TestingGo/sync/sync_test.go
+++ TestingGo/sync/sync_test.go
@@ -15,4 +15,18 @@ func TestCounter(t *testing.T) {
 			t.Errorf("got %d want %d", counter.Value(), 3)
 		}
 	})
+
+	t.Run("incrementing the counter 4 times and leaves it at 4", func(t *testing.T) {
+		counter := Counter{}
+
+		// increment the counter 4 times
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		if counter.Value() != 4 {
+			t.Errorf("got %d want %d", counter.Value(), 4)
+		}
+	})
 }
