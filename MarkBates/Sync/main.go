3b4c53f90b8b750d84b320f44f61e3fe6acec6e3
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-14 10:58:33 +0530

    write two goroutines to print some

---
+++ MarkBates/Sync/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go func() {
+		fmt.Println("func 1")
+		time.Sleep(2 * time.Second)
+		fmt.Println("func 1")
+	}()
+
+	go func() {
+		fmt.Println("func 2")
+		time.Sleep(1 * time.Second)
+		fmt.Println("func 2")
+	}()
+
+}
