c23c8485583dc6f0aa643ff3e761c7b5205aad8d
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-14 19:32:04 +0530

    use waitgroups

--- MarkBates/Sync/main.go
+++ MarkBates/Sync/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -9,24 +10,31 @@ func main() {
 
 	queue := make(chan string)
 
-	go func(queue chan string) {
+	var w sync.WaitGroup
+
+	// wait for two goroutines
+	w.Add(2)
+
+	go func(queue chan string, w *sync.WaitGroup) {
+		defer w.Done()
 		fmt.Println("func 1")
 		time.Sleep(2 * time.Second)
 		fmt.Println("func 1")
 		queue <- "done func 1"
-	}(queue)
+	}(queue, &w)
 
-	go func(queue chan string) {
+	go func(queue chan string, w *sync.WaitGroup) {
+		defer w.Done()
 		fmt.Println("func 2")
 		time.Sleep(1 * time.Second)
 		fmt.Println("func 2")
 		queue <- "done func 2"
+	}(queue, &w)
+
+	go func(queue chan string) {
+		for text := range queue {
+			fmt.Println(text)
+		}
 	}(queue)
-
-	// fmt.Println(<-queue)
-	// fmt.Println(<-queue)
-
-	for text := range queue {
-		fmt.Println(text)
-	}
+	w.Wait()
 }
