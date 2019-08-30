01de97c9b4e4fdfff5bddb362b6f81190718ca39
mukulrawat1986 <mukulsrawat@gmail.com>
2019-08-31 01:00:12 +0530

    use reflection to get the string in the struct

--- TestingGo/reflection/reflection.go
+++ TestingGo/reflection/reflection.go
@@ -1,5 +1,13 @@
 package _reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(string)) {
-	fn("Hello World")
+	// get the concrete value stored in the interface
+	val := reflect.ValueOf(x)
+
+	// Get the 0th field of val, it panics if val's kind is not struct or index is out
+	// of range
+	field := val.Field(0)
+	fn(field.String())
 }
