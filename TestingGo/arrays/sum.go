1954676eda3534d46b81fe401e43645cdb9e659f
mukulrawat1986 <mukulsrawat@gmail.com>
2019-09-25 19:03:22 +0530

    write test for the SumAll function which takes a varying number of slices and returns a new slice containing the sum of the slices. Declare the SumAll function

--- TestingGo/arrays/sum.go
+++ TestingGo/arrays/sum.go
@@ -1,10 +1,15 @@
 package arrays
 
-// Sum returns the sum of an array of integers of length 5
+// Sum returns the sum of a slice
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll returns a slice which contains the sum of slices
+func SumAll(numbersToSum ...[]int) []int {
+	return []int{}
 }
