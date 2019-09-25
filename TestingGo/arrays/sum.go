27fac7cec2e2939afa07f22c10b491cdbd9ae597
mukulrawat1986 <mukulsrawat@gmail.com>
2019-09-25 18:51:51 +0530

    change the function and test to use slices

--- TestingGo/arrays/sum.go
+++ TestingGo/arrays/sum.go
@@ -1,7 +1,7 @@
 package arrays
 
 // Sum returns the sum of an array of integers of length 5
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
