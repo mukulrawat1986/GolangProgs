861daa26944e54ff0371a79860ab6328dab96ebe
mukulrawat1986 <mukulsrawat@gmail.com>
2019-09-25 18:50:20 +0530

    implement the Sum function

--- TestingGo/arrays/sum.go
+++ TestingGo/arrays/sum.go
@@ -1,6 +1,10 @@
 package arrays
 
 // Sum returns the sum of an array of integers of length 5
-func Sum(numbersToSum [5]int) int {
-	return 0
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
 }
