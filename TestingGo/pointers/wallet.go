c7f4b8bee1352a534a44ef4f5196cedc4381a945
mukulrawat1986 <mukulsrawat@gmail.com>
2019-09-29 14:42:06 +0530

    declare the wallet type and its methods

---
+++ TestingGo/pointers/wallet.go
@@ -0,0 +1,15 @@
+package pointers
+
+// Wallet type to represent a bitcoin wallet
+type Wallet struct {
+}
+
+// Deposit method to deposit bitcoins in the wallet
+func (w Wallet) Deposit(amount int) {
+
+}
+
+// Balance method to find the balance in the wallet
+func (w Wallet) Balance() int {
+	return 0
+}
