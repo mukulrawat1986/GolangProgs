44afd196f24e144abc07da5e6353fce9f3c1ed0d
mukulrawat1986 <mukulsrawat@gmail.com>
2019-09-30 21:10:06 +0530

    create new Bitcoin type from existing int type

--- TestingGo/pointers/wallet.go
+++ TestingGo/pointers/wallet.go
@@ -1,16 +1,19 @@
 package pointers
 
+// Bitcoin type
+type Bitcoin int
+
 // Wallet type to represent a bitcoin wallet
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit method to deposit bitcoins in the wallet
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance method to find the balance in the wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
