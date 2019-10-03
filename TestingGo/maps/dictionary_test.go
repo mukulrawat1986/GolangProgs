290275095094eaca05f1acd218f7d8cc2fc30af7
mukulrawat1986 <mukulsrawat@gmail.com>
2019-10-03 10:49:00 +0530

    use the returned error in our test

--- TestingGo/maps/dictionary_test.go
+++ TestingGo/maps/dictionary_test.go
@@ -25,8 +25,9 @@ func TestAdd(t *testing.T) {
 		dictionary := Dictionary{}
 		word := "test"
 		definition := "this is just a test"
-		dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
+		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, definition)
 	})
 }
